618f49dbb67538ab5cfc010317aa7f10743139af
rokk1 <rk0881@student.uni-lj.si>
2019-11-12 22:33:37 +0100

    Programming

--- 1_sem_nal/naloga1/Naloga1.java
+++ 1_sem_nal/naloga1/Naloga1.java
@@ -12,27 +12,54 @@ public class Naloga1 {
         
         in = new Scanner(file);
 
+        // READING INPUT DATA FROM FILE
+
         String wh = in.next();
         String[] a = wh.split(",");
         
-        int width = Integer.parseInt(a[0]);
-        int height = Integer.parseInt(a[1]);
-        System.out.println(width);
-        System.out.println(height);
+        int stVrstic = Integer.parseInt(a[0]);
+        int stStolpcev = Integer.parseInt(a[1]);
 
-        char[][] arr = new char[width][height];
+        char[][] arr = new char[stVrstic][stStolpcev];
 
-        for (int i = 0; i < width; i++) {
-            String line = in.nextLine();
-            String[] lineArr = line.split(",");
-            for (int j = 0; j < height; j++) {
-                arr[i][j] = lineArr[j].charAt(0);
+        for (int i = 0; i < stVrstic; i++) {
+            String line = in.next();
+            
+            String[] parsed = line.split(",");
+            for (int j = 0; j < parsed.length; j++) {
+                arr[i][j] = parsed[j].charAt(0);
             }
         }
 
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
-                System.out.printf("%c, ", arr[i][j]);
+        int stBesed = in.nextInt();
+        String[] besede = new String[stBesed];
+
+        for (int i = 0; i < stBesed; i++) {
+            besede[i] = in.next();
+        }
+
+
+        // BEGIN
+        int start_i = -1;
+        int start_j = -1;
+        int end_i = -1;
+        int end_j = -1;
+        print(arr);
+        
+        for (int i = 0; i < besede.length; i++) {
+            // FIND EVERY WORD INTO PUZZLE
+        }
+
+    }
+
+    public static void find() {
+        
+    }
+
+    public static void print(char[][] a) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[0].length; j++) {
+                System.out.printf("%c ", a[i][j]);
             }
             System.out.println();
         }
--- 1_sem_nal/naloga2/Naloga2.java
+++ 1_sem_nal/naloga2/Naloga2.java
@@ -1,7 +1,75 @@
-public class Naloga2 {
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
+public class Naloga2 {      // OVERHAND SHUFFLE
 
-    public static void main(String[] args) {
+    public static Scanner in;
+
+    public static void main(String[] args) throws FileNotFoundException {
         
+        File file = new File(args[0]);
+
+        in = new Scanner(file);
+
+        String stevilke = in.next();
+        String[] st = stevilke.split(",");
+        int stKart = Integer.parseInt(st[0]);
+        int stMesanj = Integer.parseInt(st[1]);
+
+        String zacZaporedje = in.next();
+        String[] karte = zacZaporedje.split(",");
+
+        LinkedList cards = new LinkedList();
+        cards.first.next = null;
+
+        for (int i = 0; i < karte.length; i++) {
+            cards.first.next = new LinkedListElement(karte[i]);
+            cards.first = cards.first.next;
+        }
+
+
+        String directions = in.next();
+        String[] navodila = directions.split(",");
+
+        printList(cards);
+
+
+    }
+
+    public static void printList(LinkedList l) {
+        LinkedListElement first = l.first;
+        while (first.next != null) {
+            System.out.printf("%s - ", first.card);
+            first = first.next;
+        }
+    }
+}
+
+class LinkedList {
+
+    LinkedListElement first;
+
+    public LinkedList() {
+        first = null;
+    }
+
+    public LinkedList(LinkedListElement prvi) {
+        first = prvi;
+    }
+}
+
+class LinkedListElement {
+    String card;
+    LinkedListElement next;
+
+    public LinkedListElement(String karta) {
+        card = karta;
+        next = null;
+    }
+
+    public LinkedListElement(String karta, LinkedListElement next1) {
+        card = karta;
+        next = next1;
     }
 }
